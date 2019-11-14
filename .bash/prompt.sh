975fd2358c09eff26799f93002ab53177fb61e95
Michael Lamertz <michael.lamertz@gmail.com>
2019-11-14 13:29:29 +0100

    Only set colors when on a tty

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -2,21 +2,25 @@ TIER="prod"
 [[ $HOSTNAME =~ -(stage|int)- ]] && TIER="int"
 [[ $HOSTNAME =~ -(test|testing)- ]] && TIER="test"
 
-BOLD="$(tput bold)"
-RED="$(tput setaf 1)"
-GREEN="$(tput setaf 2)"
-YELLOW="$(tput setaf 3)"
-BLUE="$(tput setaf 4)"
-MAGENTA="$(tput setaf 5)"
-CYAN="$(tput setaf 6)"
-WHITE="$(tput setaf 7)"
-NOCOLOR="$(tput sgr0)"
+if [ -t 1 ]; then
+    BOLD="$(tput bold)"
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    MAGENTA="$(tput setaf 5)"
+    CYAN="$(tput setaf 6)"
+    WHITE="$(tput setaf 7)"
+    NOCOLOR="$(tput sgr0)"
 
-HOSTCOLOR="${BOLD}${RED}"
-[ "$TIER" == "int" ] && HOSTCOLOR="${BOLD}${YELLOW}"
-[ "$TIER" == "test" ] && HOSTCOLOR="${BOLD}${GREEN}"
+    HOSTCOLOR="${BOLD}${RED}"
+    [ "$TIER" == "int" ] && HOSTCOLOR="${BOLD}${YELLOW}"
+    [ "$TIER" == "test" ] && HOSTCOLOR="${BOLD}${GREEN}"
 
-USERCOLOR="${BOLD}${RED}"
-[[ ${EUID} != 0 ]] && USERCOLOR="${BOLD}${GREEN}"
+    USERCOLOR="${BOLD}${RED}"
+    [[ ${EUID} != 0 ]] && USERCOLOR="${BOLD}${GREEN}"
 
-PS1="\\[${USERCOLOR}\\]\\u@\\[${HOSTCOLOR}\\]\\h\\[${NOCOLOR}\\]:\\w\\$ "
+    PS1="\\[${USERCOLOR}\\]\\u@\\[${HOSTCOLOR}\\]\\h\\[${NOCOLOR}\\]:\\w\\$ "
+else
+    PS1='\h:\w\$ '
+fi
