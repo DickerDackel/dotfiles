6ab20b385ebbb3575c672664a2c02693fafdeee0
Michael Lamertz <michael.lamertz@gmail.com>
2019-11-14 13:31:39 +0100

    Cleanups and reorgs
        * imported _monitor_host from my local settings
    	This recipe creates a 3 pane window with dstat, htop and the
    	followed journal
    
        * Imported _tmux_bundle from my local settings
    	starts various recipes each in its own window
    
        * Fixed quoting in "work" recipe
    
        * Removed recipes specific to my local network out of here

--- .bash/tmux.sh
+++ .bash/tmux.sh
@@ -39,6 +39,32 @@ tsk () {
     tmux send-keys "$@" C-j
 }
 
+_monitor_host () {
+    local host
+    host=$1; shift 1
+
+    tmux rename-window $host
+    tmux split-window -v -p 20 "ssh root@$host"
+    tsk "journalctl -f | ccze -A -o nolookups"
+    tmux select-pane -t0
+    tmux split-window -h -p 45 "ssh root@$host"
+    tsk "htop"
+    tmux select-pane -t0
+    tsk "ssh root@$host"
+    tsk "dstat -larm"
+}
+
+_tmux_bundle () {
+    for recipe in "$@"; do
+	tmux new-window -n "$recipe"
+	tm "$recipe"
+    done
+}
+
+_tmux_monitor () {
+    _monitor_host "$@"
+}
+
 # Just a vertical split to a named host (or salt by default)
 _tmux_ssh () {
     tmux split-window -v "ssh ${1:salt}"
@@ -47,33 +73,20 @@ _tmux_ssh () {
 # Just 2 big root panes
 _tmux_work () {
     tmux split-window -h
-    tsk -t 1 sudo bash
-    tsk -t 0 sudo bash
+    tsk -t 1 "sudo bash"
+    tsk -t 0 "sudo bash"
     tmux select-pane 0
 }
 
-# My nginx log session
-# Win 1: cache-hit log
-# Win 2: goaccess
-_tmux_nginx () {
-    [ -z "$TMUX" ] && tmux new-session -d -s nginx -n cache-hits 'ssh nginx'
-    tsk -t nginx:cache-hits 'exec sudo bash'
-    tsk -t nginx:cache-hits 'exec /usr/local/bin/cache-hits'
-
-    tmux new-window -t nginx -n goaccess 'ssh nginx'
-    tsk -t nginx:goaccess 'exec sudo bash'
-    tsk -t nginx:goaccess 'cd /srv/goaccess'
-    tsk -t nginx:goaccess 'exec ./runme'
-
-    [ -z "$TMUX" ] && tmux attach -t nginx
-}
-
+# Usage example
+#   activate with "tm salt"
 # 4 Panes to work with saltstack
 #   0. Major edit pane
 #   1. run salt commands
 #   2. ssh to target host (tm salt TARGET-HOST), also salt if empty
 #   3. colortail on /var/log/messages, /var/log/salt/master & minion
 _tmux_salt () {
+    tmux rename-window salt
     # top right: small shell on target host (or salt)
     tmux split-window -h -l 100 "ssh ${1:-salt}"
     tsk -t 1 'sudo bash'
@@ -81,7 +94,7 @@ _tmux_salt () {
     tsk -t 1 'ls'
     #tsk -t 1 C-l 'ls'
 
-    # bottom right: logs
+    # bottom right: logs (see ccze.sh)
     tmux split-window -v 'ssh salt'
     tsk -t 2 'sudo bash'
     tsk -t 2 'ct messages /var/log/salt/{minion,master}'
@@ -99,49 +112,6 @@ _tmux_salt () {
     tsk -t 0 'sudo bash'
     tsk -t 0 'cd /srv/salt'
     tsk -t 0 C-l 'ls'
-}
-
-# Modified 'salt' layout with one window on the lxc host to handle container
-# stuff
-#   0. Major edit pane
-#   1. shuttle:/srv/lxc
-#   2. ssh to target host (tm salt TARGET-HOST), also salt if empty
-#   3. colortail on /var/log/messages, /var/log/salt/master & minion
-_tmux_newhost () {
-    # top right: small shell on target host (or salt)
-    tmux split-window -h -l 100 "ssh ${1:-salt}"
-    tsk -t 1 'sudo bash'
-    tsk -t 1 'cd /srv/salt'
-    tsk -t 1 'ls'
-    #tsk -t 1 C-l 'ls'
-
-    # bottom right: logs
-    tmux split-window -v 'ssh salt'
-    tsk -t 2 'sudo bash'
-    tsk -t 2 'ct messages /var/log/salt/{minion,master}'
-
-    # top right: small run shell
-    tmux select-pane -t 0
-    tmux split-window -v -l 25 'ssh shuttle'
-    tsk -t 1 'sudo bash'
-    tsk -t 1 'cd /srv/lxc'
-    tsk -t 1 C-l ls C-j
-
-    # top-left: work on salt
-    tmux select-pane -t 0
-    tsk -t 0 'ssh salt'
-    tsk -t 0 'sudo bash'
-    tsk -t 0 'cd /srv/salt'
-    tsk -t 0 C-l 'ls'
-}
-
-# 3-pane layout on the dev host for free usage
-_tmux_dev () {
-    tmux new-window -n "Develop"
-    tmux split-window -h -l 100 'ssh devel'
-    tmux split-window -t 1 -v 'ssh devel'
-    tmux select-pane -t 0
-    tmk 'ssh devel'
 }
 
 _tmux_2 () {
