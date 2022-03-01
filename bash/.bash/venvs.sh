37fedd1eb1fb4ba828c5b566f193a8ec8ce5ed49
Michael Lamertz <michael.lamertz@gmail.com>
2022-03-01 06:45:34 +0100

    venv now handles Windows and Linux

--- bash/.bash/venvs.sh
+++ bash/.bash/venvs.sh
@@ -1,5 +1,27 @@
-alias activate="source .venv/bin/activate"
+_activate_usage () {
+    cat <<-EOF
+	usage: activatet [venv]
 
+	    venv		virtual env directory, default .venv or venv
+
+	Activate virtualenv from venv
+EOF
+}
+
+activate () {
+    if [ $# == 1 ]; then
+	venv=$1
+    else
+	for d in .venv venv; do
+	    [ -d "$d" ] && venv="$d" && break
+	done
+	[ -z "$venv" ] && _activate_usage && return 1
+    fi
+    for d in bin Scripts; do
+	[ -d "$venv/$d" ] && source "$venv/$d/activate" && break
+    done
+}
+	
 mkvenv () {
     name=$( basename $PWD )
     python -m venv --prompt "$name" .venv
