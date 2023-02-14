b79e2d259322b9639b4bdef0cfa424177ada7ff7
Michael Lamertz <michael.lamertz@gmail.com>
2023-02-15 00:51:08 +0100

    Again reworked venv handling

--- bash/.bash/venvs.sh
+++ bash/.bash/venvs.sh
@@ -1,2 +1,20 @@
-alias venv='. ./bin/activate'
+#alias venv='. ./bin/activate'
 
+venv () {
+    local v
+
+    for d in venv .venv .; do
+	if [ -d $d ]; then
+	    . $d/bin/activate
+	    break
+	fi
+    done
+}
+
+mkv () {
+    if [ $# == 1 ]; then
+	python -m venv "$1"
+    else
+	python -m venv .venv --prompt $( basename $( readlink -f $PWD ) )
+    fi
+}
