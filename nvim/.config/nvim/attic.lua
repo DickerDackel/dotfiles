7dc9b00ca89497c48c81f1452ccd0eba36bfaba7
Michael Lamertz <michael.lamertz@gmail.com>
2025-07-31 22:19:30 +0200

    No longer used stuff outsourced

---
+++ nvim/.config/nvim/attic.lua
@@ -0,0 +1,28 @@
+-- Why?!?
+-- set notermguicolors
+
+-- Removed, left as reminder
+-- "set showbreak=>>
+-- "set statusline=%<%f\ %h%w%m%y%r%=%-14.(%l,%c%V%)\ %P
+
+-- These are not available in neovim
+-- set esckeys
+-- set t_Co=256
+
+-- These are default in neovim
+-- vim.o.background=dark
+-- vim.o.autoread = true
+-- vim.o.termguicolors = true
+-- vim.o.backspace=indent,eol,start
+-- vim.o.foldmethod=manual
+-- vim.o.hlsearch = true
+-- vim.o.incsearch = true
+-- vim.o.laststatus=2
+-- vim.o.magic = true
+-- vim.o.modeline = true
+-- vim.o.noautochdir = true
+-- vim.o.nocompatible = true
+-- vim.o.nopaste = true
+-- vim.o.noshiftround = true
+-- vim.o.smarttab = true
+-- vim.o.wildmenu = true
