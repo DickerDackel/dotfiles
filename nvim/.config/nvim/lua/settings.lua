bfac779ba06f11f72553b4e957c862356b606345
Michael Lamertz <michael.lamertz@gmail.com>
2025-07-31 22:20:34 +0200

    Old UltiSnips settings from vim

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -48,4 +48,17 @@ vim.cmd 'runtime macros/matchit.vim'
 -- Show trailing whitespace as errors
 vim.cmd 'match errorMsg /\\s\\+$/'
 
+------------------------------------------------------------------------
+-- UltiSnips
+------------------------------------------------------------------------
+-- vim.cmd "let g:UltiSnipsSnippetStorageDirectoryForUltiSnipsEdit = '~/.config/nvim/ultisnips'"
+-- vim.cmd "let g:UltiSnipsSnippetDirectories=[$HOME.'/.config.nvim/ultisnips']"
+vim.cmd "let g:UltiSnipsSnippetDirectories = ['UltiSnips', 'ultisnips']"
+vim.cmd "let g:UltiSnipsExpandTrigger = '<tab>'"
+vim.cmd "let g:UltiSnipsListSnippets = '<s-tab>'"
+vim.cmd "let g:UltiSnipsJumpForwardTrigger = '<c-j>'"
+vim.cmd "let g:UltiSnipsJumpBackwardTrigger = '<c-k>'"
+vim.cmd "let g:UltiSnipsEditSplit='context'"
+vim.cmd "let g:ultisnips_python_quoting_style = 'single'"
+vim.cmd "let g:ultisnips_python_triple_quoting_style = 'double'"
 
