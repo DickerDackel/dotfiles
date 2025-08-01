08cfad053fedcea6b0309b15943edf525a7f026c
Michael Lamertz <michael.lamertz@gmail.com>
2025-08-01 23:26:06 +0200

    neovim, replace telescope with snacks?  Other stuff too

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,41 +2,44 @@ require("config.lazy")
 require("settings")
 require("bindings")
 require("colors")
-require("questionable_kickstart_configs")
+-- require("questionable_kickstart_configs")
 
 -- Don't use automatic includes.  I want control and sometimes keep config
 -- files in the directory because I might re-enable them.
 require("lazy").setup({
-    require("plugins.kickstart.blink"),
-    require("plugins.kickstart.init"),
-    require("plugins.kickstart.lazydev"),
-    require("plugins.kickstart.mini"),
-    require("plugins.kickstart.nvim-lspconfig"),
-    require("plugins.kickstart.nvim-treesitter"),
-    require("plugins.kickstart.telescope"),
-    require("plugins.kickstart.todo-comments"),
-    require("plugins.kickstart.which-key"),
+	require("plugins.kickstart.blink"),
+	require("plugins.kickstart.fidget"),
+	require("plugins.kickstart.init"),
+	require("plugins.kickstart.lazydev"),
+	require("plugins.kickstart.mini"),
+	require("plugins.kickstart.nvim-lspconfig"),
+	require("plugins.kickstart.nvim-treesitter"),
+	-- require("plugins.kickstart.telescope"),
+	require("plugins.kickstart.todo-comments"),
+	require("plugins.kickstart.which-key"),
 
-    require("plugins.approved.commentary"),
-    require("plugins.approved.fugitive"),
-    require("plugins.approved.fzf"),
-    require("plugins.approved.markdown"),
-    -- require("plugins.approved.pep8indent"),
-    require("plugins.approved.startify"),
-    require("plugins.approved.tagbar"),
-    require("plugins.approved.ultisnips"),
+	require("plugins.approved.commentary"),
+	require("plugins.approved.fugitive"),
+	require("plugins.approved.fzf"),
+	require("plugins.approved.markdown"),
+	require("plugins.approved.pep8indent"),
+	require("plugins.approved.startify"),
+	require("plugins.approved.tagbar"),
+	require("plugins.approved.ultisnips"),
 
-    require("plugins.testing.align"),
-    require("plugins.testing.harpoon"),
-    require("plugins.testing.lualine"),
-    require("plugins.testing.neodev"),
-    require("plugins.testing.tree"),
-    require("plugins.testing.trouble"),
+	require("plugins.testing.align"),
+	require("plugins.testing.conform"),
+	require("plugins.testing.dap"),
+	require("plugins.testing.harpoon"),
+	require("plugins.testing.lualine"),
+	require("plugins.testing.neodev"),
+	require("plugins.testing.snacks"),
+	require("plugins.testing.tree"),
+	require("plugins.testing.trouble"),
 
-    require("plugins.testing.autoformat"),
-    -- require("plugins.kickstart.git-signs"),
+	-- require("plugins.kickstart.git-signs"),
 
-    install = { colorscheme = { "vividchalk", "habamax" } },
+	install = { colorscheme = { "vividchalk"} },
 })
 
 require("lualine").setup({
@@ -51,3 +54,10 @@ vim.diagnostic.config({
     signs = true,
     jump = { float = true },
 })
+
+-- vim.keymap.set({'n', 'v'}, '<Leader>f', vim.lsp.buf.format, bufopts)
+vim.keymap.set({'n', 'v'}, '<leader>f', function()
+  vim.lsp.buf.format({ })
+end , bufopts)
+
+require("dap-python").setup("uv")
--- nvim/.config/nvim/lua/plugins/testing/conform.lua
+++ nvim/.config/nvim/lua/plugins/testing/conform.lua
@@ -1,26 +1,23 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    cmd = { "ConformInfo" },
-    opts = {
-	notify_on_error = false,
-	formatters_by_ft = {
-	    lua = { "stylua" },
-	    -- python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-	    -- python = { "autopep8" },
-	    -- python = { "blue" },
-	    -- python = { "pyink" },
-	},
-	format_on_save = function(bufnr) return nil end,
-	keys = {
-	    {
-		"<leader>f",
-		function()
-		    require("conform").format({ async = true, lsp_format = "fallback" })
-		end,
-		mode = "",
-		desc = "[F]ormat buffer",
-	    },
-	},
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  cmd = { "ConformInfo" },
+  opts = {
+    notify_on_error = false,
+    formatters_by_ft = {
+      -- python = { "ruff", "ruff_organize_imports", "ruff_format" },
+      lua = { "stylua" },
     },
+    format_on_save = function(bufnr) return nil end,
+    -- keys = {
+    --   {
+	-- "<leader>f",
+	-- function()
+	  -- require("conform").format({ async = true, lsp_format = "fallback" })
+	-- end,
+	-- mode = "",
+	-- desc = "[F]ormat buffer",
+    --   },
+    -- },
+  },
 }
