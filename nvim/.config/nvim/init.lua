1b7622432bebe4f1c784cf498d6a7b0b9049486d
Michael Lamertz <michael.lamertz@gmail.com>
2025-08-06 22:16:14 +0200

    Reordered stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,44 +7,48 @@ require("colors")
 -- Don't use automatic includes.  I want control and sometimes keep config
 -- files in the directory because I might re-enable them.
 require("lazy").setup({
-	require("plugins.kickstart.blink"),
-	require("plugins.kickstart.fidget"),
-	require("plugins.kickstart.init"),
-	require("plugins.kickstart.lazydev"),
-	require("plugins.kickstart.mini"),
-	require("plugins.kickstart.nvim-lspconfig"),
-	require("plugins.kickstart.nvim-treesitter"),
-	-- require("plugins.kickstart.telescope"),
-	require("plugins.kickstart.todo-comments"),
-	require("plugins.kickstart.which-key"),
+  require("plugins.testing.neodev"),
 
-	require("plugins.approved.commentary"),
-	require("plugins.approved.fugitive"),
-	require("plugins.approved.fzf"),
-	require("plugins.approved.markdown"),
-	require("plugins.approved.pep8indent"),
-	require("plugins.approved.startify"),
-	require("plugins.approved.tagbar"),
-	require("plugins.approved.ultisnips"),
+  require("plugins.testing.align"),
+  require("plugins.testing.conform"),
+  require("plugins.testing.dap"),
+  require("plugins.testing.harpoon"),
+  require("plugins.testing.lualine"),
+  require("plugins.testing.obsidian"),
+  require("plugins.testing.snacks"),
+  require("plugins.testing.tree"),
+  require("plugins.testing.trouble"),
 
-	require("plugins.testing.align"),
-	require("plugins.testing.conform"),
-	require("plugins.testing.dap"),
-	require("plugins.testing.harpoon"),
-	require("plugins.testing.lualine"),
-	require("plugins.testing.neodev"),
-	require("plugins.testing.snacks"),
-	require("plugins.testing.tree"),
-	require("plugins.testing.trouble"),
+  require("plugins.kickstart.blink"),
+  require("plugins.kickstart.fidget"),
+  require("plugins.kickstart.init"),
+  require("plugins.kickstart.lazydev"),
+  require("plugins.kickstart.mini"),
+  require("plugins.kickstart.nvim-lspconfig"),
+  require("plugins.kickstart.nvim-treesitter"),
+  require("plugins.kickstart.telescope"),
+  require("plugins.kickstart.todo-comments"),
+  require("plugins.kickstart.which-key"),
 
-	-- require("plugins.kickstart.git-signs"),
+  require("plugins.approved.commentary"),
+  require("plugins.approved.fugitive"),
+  require("plugins.approved.fzf"),
+  require("plugins.approved.markdown"),
+  require("plugins.approved.pep8indent"),
+  require("plugins.approved.startify"),
+  require("plugins.approved.tagbar"),
+  require("plugins.approved.ultisnips"),
 
-	install = { colorscheme = { "vividchalk"} },
+  -- require("plugins.kickstart.git-signs"),
+
+  install = { colorscheme = { "vividchalk"} },
+  install = { colorscheme = { "golden"} },
+  install = { colorscheme = { "ayu"} }, -- Fix background
 })
 
-require("lualine").setup({
-    options = { theme = "ayu_dark" },
-})
+-- require("lualine").setup({
+--     options = { theme = "ayu_dark" },
+-- })
 
 vim.diagnostic.config({
     virtual_text = false,
@@ -60,4 +64,6 @@ vim.keymap.set({'n', 'v'}, '<leader>f', function()
   vim.lsp.buf.format({ })
 end , bufopts)
 
-require("dap-python").setup("uv")
+vim.keymap.set('n', '<leader>/', function () Snacks.terminal() end)
+
+-- vim.lsp.set_log_level("TRACE")
