2caab0d3227137e0c010664e2242770d5cd339d0
Michael Lamertz <michael.lamertz@gmail.com>
2025-08-11 13:40:54 +0200

    Try neo-tree instead of nvim-tree

--- nvim/.config/nvim/lua/plugins/testing/tree.lua
+++ nvim/.config/nvim/lua/plugins/testing/tree.lua
@@ -1,11 +1,35 @@
+-- return {
+--   "nvim-tree/nvim-tree.lua",
+--   version = "*",
+--   lazy = false,
+--   dependencies = {
+--     "nvim-tree/nvim-web-devicons",
+--   },
+--   config = function()
+--     require("nvim-tree").setup {}
+--   end,
+-- }
+
+
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
+  lazy = false,
+  keys = {
+    { '<leader>T', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['<leader>T'] = 'close_window',
+        },
+      },
+    },
+  },
 }
