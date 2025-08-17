46eaf631fca5e33dac01aa9b12a818ffe3cc558a
Michael Lamertz <michael.lamertz@gmail.com>
2025-08-17 19:37:08 +0200

    removed commented out old plugin

--- nvim/.config/nvim/lua/plugins/approved/tree.lua
+++ nvim/.config/nvim/lua/plugins/approved/tree.lua
@@ -1,16 +1,3 @@
--- return {
---   "nvim-tree/nvim-tree.lua",
---   version = "*",
---   lazy = false,
---   dependencies = {
---     "nvim-tree/nvim-web-devicons",
---   },
---   config = function()
---     require("nvim-tree").setup {}
---   end,
--- }
-
-
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
