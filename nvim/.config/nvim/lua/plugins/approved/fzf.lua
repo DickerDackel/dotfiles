8c9ba921b2e13aed682916c4beb259c130ee2b26
Michael Lamertz <michael.lamertz@gmail.com>
2025-08-19 13:02:11 +0200

    commented out shit removed

--- nvim/.config/nvim/lua/plugins/approved/fzf.lua
+++ nvim/.config/nvim/lua/plugins/approved/fzf.lua
@@ -1,9 +1,5 @@
--- fzf fuzzy finder
--- return { 'junegunn/fzf.vim', dependencies = { 'junegunn/fzf' } }
 return { "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- or if using mini.icons/mini.nvim
-  -- dependencies = { "echasnovski/mini.icons" },
   opts = {
     'fzf-vim',
     files = {
@@ -11,22 +7,16 @@ return { "ibhagwan/fzf-lua",
       follow = true,  -- follow symlinks
       no_ignore = false,  -- Do not not use .gitignore
 
-      -- cmd = "~/.local/bin/fdwrapper",
       -- NOTE: --type f will disable .gitignore.  Must be reenabled with
       -- --ignore
       fd_opts = [[--color=never --type f --type l --ignore]],
-      -- cmd = "fd --color=never --type f --type l --ignore --follow",
-      -- fd_opts = [[--color=never --type f --type l --ignore]],
     },
     grep = {
-      hidden = false,  -- fzf-lua injects --hidden into finders by default!
-      follow = true,  -- follow symlinks
-      no_ignore = false,  -- Do not not use .gitignore
+      hidden = false,
+      follow = true,
+      no_ignore = false,
 
-      -- cmd = "~/.local/bin/fdwrapper",
       fd_opts = [[--color=never]],
-      -- cmd = "fd --color=never --type f --type l --ignore --follow",
-      -- fd_opts = [[--color=never --type f --type l --ignore]],
     },
   },
 }
