04587b40bb1f7e2761563f2339c7e324d9f38265
Michael Lamertz <michael.lamertz@gmail.com>
2025-08-25 14:18:20 +0200

    eza ls compatibility function stolen from github

--- bash/.bash/eza.sh
+++ bash/.bash/eza.sh
@@ -1,4 +1,92 @@
-if type eza >/dev/null 2>&1; then
-    alias ls=eza
-    alias ll='eza -lar -s modified'
-fi
+alias ls=eza_wrapper
+alias ll='eza_wrapper -l'
+
+# Stolen here: https://github.com/eza-community/eza/issues/980#issuecomment-2499761779
+eza_wrapper () {
+    args=()
+    while [ "$#" -gt 0 ]; do
+        arg="$1"
+        shift
+        case "$arg" in
+            --)
+                args+=("$arg")
+                break;
+                ;;
+            --directory)
+                args+=("--list-dirs")
+                ;; # Replace --directory with --list-dirs
+            --numeric-uid-gid)
+                args+=("--numeric")
+                ;; # Replace --numeric-uid-gid with --numeric
+            --block-size | --author | --escape | --dired | --full-time | --kibibytes | --literal | --quoting-style | --size | --time-style | --width)
+                if [ "$#" -gt 0 ] && [ "${1#-}" = "$1" ]; then
+                    shift
+                fi
+                ;;
+            --sort | --sort=* | --time | --time=* | --indicator-style | --indicator-style=* | --color | --color=* | --hyperlink | --hyperlink=*)
+                if [ "${arg#*=}" != "$arg" ]; then
+                    args+=("$arg")
+                else
+                    if [ "$#" -gt 0 ] && [ "${1#-}" = "$1" ]; then
+                        args+=("$arg" "$1")
+                        shift
+                    else
+                        args+=("$arg")
+                    fi
+                fi
+                ;;
+            --*)
+                args+=("$arg")
+                ;;
+            -*)
+                arg="${arg#-}"
+                while [ -n "$arg" ]; do
+                    char="${arg%"${arg#?}"}"
+                    case "$char" in
+                        t)
+                            args+=("--sort=modified")
+                            ;; # Suppress -t
+                        r)
+                            args+=("--reverse")
+                            ;; # Suppress -r
+                        S)
+                            args+=("--sort=size")
+                            ;; # Supress -S
+                        s)
+                            ;; # Supress -s (no equivalent in eza)
+                        c)
+                            args+=("--sort=changed")
+                            ;; # Supress -c
+                        u)
+                            args+=("--sort=accessed")
+                            ;; # Supress -u
+                        1)
+                            args+=("--oneline")
+                            ;; # Supress -1
+                        g)
+                            args+=("--group")
+                            ;; # Supress -g
+                        d)
+                            args+=("--list-dirs")
+                            ;; # Supress -d
+                        p)
+                            args+=("--indicator-style=slash")
+                            ;; # Supress -p
+                        *)
+                            args+=("-${char}")
+                            ;;
+                    esac
+                    arg="${arg#?}"
+                done
+                ;;
+            *)
+                args+=("$arg")
+                ;;
+        esac
+    done
+
+    args+=("$@")
+
+    # Execute eza with the processed arguments
+    eza "${args[@]}"
+}
