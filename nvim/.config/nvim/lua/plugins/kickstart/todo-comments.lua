3c693437893c42c23b972f10acceb63332478111
Michael Lamertz <michael.lamertz@gmail.com>
2025-09-11 19:26:47 +0200

    Original required a ':', and I often don't have trailing text

--- nvim/.config/nvim/lua/plugins/kickstart/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/kickstart/todo-comments.lua
@@ -2,5 +2,13 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { }
+  opts = {
+    highlight = {
+      before = "",
+      after="",
+      keyword="bg",
+      pattern = [[<(KEYWORDS)>]],
+    },
+    search = { pattern = [[\b(KEYWORDS)\b]], },
+  }
 }
